592c9ee913ed5aea79d97398abba1d0beb61e135
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-03-04 17:11:01 +0600

    coroutine apply with MVVM Retrofit

--- app/src/main/java/com/shadhin/kotlincoroutines/ViewModel/ListViewModel.kt
+++ app/src/main/java/com/shadhin/kotlincoroutines/ViewModel/ListViewModel.kt
@@ -2,11 +2,18 @@ package com.shadhin.kotlincoroutines.ViewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.shadhin.kotlincoroutines.Model.CountriesService
 import com.shadhin.kotlincoroutines.Model.Country
+import kotlinx.coroutines.*
 import retrofit2.HttpException
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
 
+    val countriesService = CountriesService.getCountriesService()
+    var job: Job? = null
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        onError("Exception: ${throwable.localizedMessage}")
+    }
     val countries = MutableLiveData<List<Country>>()
     val countryLoadError = MutableLiveData<String?>()
     val loading = MutableLiveData<Boolean>()
@@ -17,15 +24,26 @@ class ListViewModel: ViewModel() {
 
     private fun fetchCountries() {
         loading.value = true
-
-        val dummyData = generateDummyCountries()
-
-        countries.value = dummyData
-        countryLoadError.value = ""
-        loading.value = false
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            val response = countriesService.getCountries()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    countries.value = response.body()
+                    countryLoadError.value = null
+                    loading.value = false
+                } else {
+                    onError("Error: ${response.message()}")
+                }
+            }
+        }
+        /*
+         val dummyData = generateDummyCountries()
+         countries.value = dummyData
+         countryLoadError.value = ""
+         loading.value = false*/
     }
 
-    private fun generateDummyCountries(): List<Country> {
+    /*private fun generateDummyCountries(): List<Country> {
         val countries = arrayListOf<Country>()
         countries.add(Country("dummyCountry1",  "dummyCapital1",""))
         countries.add(Country("dummyCountry2",  "dummyCapital2",""))
@@ -33,11 +51,16 @@ class ListViewModel: ViewModel() {
         countries.add(Country("dummyCountry4",  "dummyCapital4",""))
         countries.add(Country("dummyCountry5",  "dummyCapital5",""))
         return countries
-    }
+    }*/
 
     private fun onError(message: String) {
         countryLoadError.value = message
         loading.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
     }
 
 }
