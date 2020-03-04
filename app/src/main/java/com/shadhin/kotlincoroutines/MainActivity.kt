ac7c0e0d735d1979b76240269d5ee959e6782fda
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-03-04 15:19:50 +0600

    Dependency and init setup

--- app/src/main/java/com/shadhin/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/shadhin/kotlincoroutines/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
