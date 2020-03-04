d5cb15a88926ee913520d04403c153bc73fda1ae
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-03-04 15:41:05 +0600

    imageview By using network call

--- app/src/main/java/com/shadhin/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/shadhin/kotlincoroutines/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.shadhin.kotlincoroutines
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val IMAGE_URL =
+        "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) { getOrginalBitmap() }
+            val orginalBitmap = originalDeferred.await()
+            loadImage(orginalBitmap)
+        }
+    }
+    private fun getOrginalBitmap(): Bitmap {
+        return URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+    }
+    private fun loadImage(bmp: Bitmap) {
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bmp)
+        imageView.visibility = View.VISIBLE
     }
 }
